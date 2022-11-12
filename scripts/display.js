e255d9f32c59c1a5f26f7e29dbc3960dae8f4681
akt0001c <110126989+akt0001c@users.noreply.github.com>
2022-11-12 23:00:23 +0530

    finalcommitfp06_253day5

--- scripts/display.js
+++ scripts/display.js
@@ -1,27 +1,36 @@
 let key=`tripData`;
 
 let tripData = JSON.parse(localStorage.getItem(key));
-let hreserve= document.querySelector('#book-hotel');
-let  hotelData= JSON.parse(localStorage.getItem(key));
+let hreserve= document.querySelectorAll('.price-box>div');
+
 window.onload = ()=>{
      document.title= tripData.name;
     let himage = document.querySelector("#main-image");
-    himage.setAttribute("src","hotelData.image");
+    himage.src= tripData.image;
+    
 };
-hreserve.onclick = ()=>{
+for(let j=0;j<hreserve.length;j++)
+{
+hreserve[j].onclick = ()=>{
     let hotel_key=`hotelData`;
-    localStorage.setItem(hotel_key,JSON.stringify(tripData));
+    let hotelData = JSON.parse(localStorage.getItem(hotel_key)) ||[];
+    hotelData.push(tripData);
+    localStorage.setItem(hotel_key,JSON.stringify(hotelData));
 };
+}
 
 
 let hname= document.querySelector("#hotel-name");
 hname.textContent= tripData.name;
 
 let hrating= document.querySelector("#rating-box");
-hrating.textContent= tripData.price;
+hrating.textContent= tripData.ratings;
 
-let hprice = document.querySelector(".card-price");
-hprice.textContent= `₹${tripData.price}`;
+let hprice = document.querySelectorAll(".card-price");
+for(let i=0;i<hprice.length;i++)
+{
+hprice[i].textContent= `₹${tripData.price}`;
+}
 
 
 
